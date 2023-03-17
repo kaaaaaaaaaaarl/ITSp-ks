f2b414f99410d048096831de229bd35f22eb182b
kaaaaaaaaaaarl <97940914+kaaaaaaaaaaarl@users.noreply.github.com>
2023-03-17 08:29:04 +0200

    Add files via upload

---
+++ pps/News.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="lv">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Majaslapa</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <link rel="stylesheet" href="style_main.css">
+</head>
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Raleway&display=swap');
+  </style>
+<body>
+  <header>
+    <section class="sect1">
+      <a href="C:\html learning\school\pps\index.html" class="pageS1"> <div class="pageS"><p>Sakums</p></div> </a>
+        <a href="C:\html learning\school\pps\News.html" class="pageS1">  <div class="pageS"><p>Jaunumi</p></div> </a>
+        <a href="C:\html learning\school\pps\Vakances.html" class="pageS1"> <div class="pageS"><p>Vakances</p></div></a>
+        <a href="#Kontakti" class="pageS1">  <div class="pageS"><p>Kontakti</p></div></a>
+    </section>
+
+    <section class="sect2">
+      <a href="#popup" class="pageS1">  <div class="pageS"><p>Ielogoties</p></div></a>
+
+      <div class="popup" id="popup">
+        <a href="#" class="close">Aizvert</a>
+        <h2>Ielogoties</h2>
+        <form class="form2">
+          <label for="username">Lietotajvārds:</label>
+          <input type="text" id="username" name="username"><br><br>
+          <label for="password">Parole:</label>
+          <input type="password" id="password" name="password"><br><br>
+          <input type="submit" value="Submit">
+        </form>
+      </div>
+    </section>
+  </header>
+  <div class="mainPage">
+    
+
+    <div class="pnews">
+      <h1>Jaunumi</h1>
+      <div class="item">
+        <img src="https://scontent.frix6-1.fna.fbcdn.net/v/t39.30808-6/334118685_2385932744945229_3420852421440632462_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_ohc=izEJ1FIzVHkAX_4ZEXs&_nc_ht=scontent.frix6-1.fna&oh=00_AfATgPRzIP22Afx6ZRpZuCftMBc5ChtSe79-tJE90oCOyw&oe=640DFE1A" alt="Image 1">
+        <div class="text">
+          <h2>IT Waffle Meetup #24.</h2>
+          <p>Šobrīd nav pateikts uz kuru datumu notiks Nākošais Waffle meetup. Lai gan vienmēr ir laba ideja aiziet us šiem pasākumiem kas ir organizēti caur "Liepājas Biznesa Inkubātora LIAA" Lai atrastu vakances, draugus vai pat pilnus darbus. Lai izlasītu vairāk varat apmeklēt <a href="https://www.facebook.com/ITWaffleMeetup/">facebook.com/ITWaffleMeetup</a></p>
+        </div>
+      </div>
+      <div class="item">
+        <img src="https://via.placeholder.com/600x400" alt="Image 2">
+        <div class="text">
+          <h2>Ziņas 2</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce convallis, purus eu tempor pharetra, arcu velit consectetur massa, vel dapibus turpis est at arcu.</p>
+        </div>
+      </div>
+      <div class="item">
+        <img src="https://via.placeholder.com/600x400" alt="Image 3">
+        <div class="text">
+          <h2>Ziņas 3</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce convallis, purus eu tempor pharetra, arcu velit consectetur massa, vel dapibus turpis est at arcu.</p>
+        </div>
+      </div>
+      <div class="item">
+        <img src="https://via.placeholder.com/600x400" alt="Image 3">
+        <div class="text">
+          <h2>Ziņas 4</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce convallis, purus eu tempor pharetra, arcu velit consectetur massa, vel dapibus turpis est at arcu.</p>
+        </div>
+      </div>
+      <div class="item">
+        <img src="https://via.placeholder.com/600x400" alt="Image 3">
+        <div class="text">
+          <h2>Ziņas 5</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce convallis, purus eu tempor pharetra, arcu velit consectetur massa, vel dapibus turpis est at arcu.</p>
+        </div>
+      </div>
+      <div class="item">
+        <img src="https://via.placeholder.com/600x400" alt="Image 3">
+        <div class="text">
+          <h2>Ziņas 6</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce convallis, purus eu tempor pharetra, arcu velit consectetur massa, vel dapibus turpis est at arcu.</p>
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+
+
+  <footer id="Kontakti">
+    <div class="social-media-icons">
+      <a href="#"><i class="fab fa-facebook"></i></a>
+      <a href="#"><i class="fab fa-twitter"></i></a>
+      <a href="#"><i class="fab fa-instagram"></i></a>
+      <a href="#"><i class="fab fa-linkedin"></i></a>
+    </div>
+    <div class="contact-us">
+      <h3>Kontakti</h3>
+      <p>Ē-pasts: ITspeks@gmail.com</p>
+      <p>Telefon Nummurs: 555-555-5555</p>
+    </div>
+  </footer>
+</body>
+</html>
---
+++ pps/vakances.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="lv">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Majaslapa</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <link rel="stylesheet" href="style_main.css">
+</head>
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Raleway&display=swap');
+  </style>
+<body>
+  <header>
+    <section class="sect1">
+      <a href="C:\html learning\school\pps\index.html" class="pageS1"> <div class="pageS"><p>Sakums</p></div> </a>
+      <a href="C:\html learning\school\pps\News.html" class="pageS1">  <div class="pageS"><p>Jaunumi</p></div> </a>
+      <a href="C:\html learning\school\pps\Vakances.html" class="pageS1"> <div class="pageS"><p>Vakances</p></div></a>
+      <a href="#Kontakti" class="pageS1">  <div class="pageS"><p>Kontakti</p></div></a>
+    </section>
+
+    <section class="sect2">
+      <a href="#popup" class="pageS1">  <div class="pageS"><p>Ielogoties</p></div></a>
+
+      <div class="popup" id="popup">
+        <a href="#" class="close">Aizvert</a>
+        <h2>Ielogoties</h2>
+        <form class="form2">
+          <label for="username">Lietotajvārds:</label>
+          <input type="text" id="username" name="username"><br><br>
+          <label for="password">Parole:</label>
+          <input type="password" id="password" name="password"><br><br>
+          <input type="submit" value="Submit">
+        </form>
+      </div>
+    </section>
+  </header>
+
+
+
+
+  <div class="mainPage">
+    
+
+    <div class="vakancez">
+      <h1>Vakances</h1>
+      <div class="item">
+        <img src="https://via.placeholder.com/600x400" alt="Image 1">
+        <div class="text">
+          <h2>Vakance 1</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce convallis, purus eu tempor pharetra, arcu velit consectetur massa, vel dapibus turpis est at arcu.</p>
+          
+          <h2><a style="font-size: 20px;" href="PieteiktiesVak.html" class="pieteikties">Pieteikties</a></h2>
+            
+        </div>
+      </div>
+      <div class="item">
+        <img src="https://via.placeholder.com/600x400" alt="Image 2">
+        <div class="text">
+          <h2>Vakance 2</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce convallis, purus eu tempor pharetra, arcu velit consectetur massa, vel dapibus turpis est at arcu.</p>
+          <h2><a style="font-size: 20px;" href="PieteiktiesVak.html" class="pieteikties">Pieteikties</a></h2>
+        </div>
+      </div>
+      <div class="item">
+        <img src="https://via.placeholder.com/600x400" alt="Image 3">
+        <div class="text">
+          <h2>Vakance 3</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce convallis, purus eu tempor pharetra, arcu velit consectetur massa, vel dapibus turpis est at arcu.</p>
+          <h2><a style="font-size: 20px;" href="PieteiktiesVak.html" class="pieteikties">Pieteikties</a></h2>
+        </div>
+      </div>
+      <div class="item">
+        <img src="https://via.placeholder.com/600x400" alt="Image 3">
+        <div class="text">
+          <h2>Vakance 4</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce convallis, purus eu tempor pharetra, arcu velit consectetur massa, vel dapibus turpis est at arcu.</p>
+          <h2><a style="font-size: 20px;" href="PieteiktiesVak.html" class="pieteikties">Pieteikties</a></h2>
+        </div>
+      </div>
+      <div class="item">
+        <img src="https://via.placeholder.com/600x400" alt="Image 3">
+        <div class="text">
+          <h2>Vakance 5</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce convallis, purus eu tempor pharetra, arcu velit consectetur massa, vel dapibus turpis est at arcu.</p>
+          <h2><a style="font-size: 20px;" href="PieteiktiesVak.html" class="pieteikties">Pieteikties</a></h2>
+        </div>
+      </div>
+      <div class="item">
+        <img src="https://via.placeholder.com/600x400" alt="Image 3">
+        <div class="text">
+          <h2>Vakance 6</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce convallis, purus eu tempor pharetra, arcu velit consectetur massa, vel dapibus turpis est at arcu.</p>
+          <h2><a style="font-size: 20px;" href="PieteiktiesVak.html" class="pieteikties">Pieteikties</a></h2>
+        </div>
+      </div>
+      
+      
+    </div>
+
+
+  </div>
+
+
+  <footer id="Kontakti">
+    <div class="social-media-icons">
+      <a href="#"><i class="fab fa-facebook"></i></a>
+      <a href="#"><i class="fab fa-twitter"></i></a>
+      <a href="#"><i class="fab fa-instagram"></i></a>
+      <a href="#"><i class="fab fa-linkedin"></i></a>
+    </div>
+    <div class="contact-us">
+      <h3>Kontakti</h3>
+      <p>Ē-pasts: ITspeks@gmail.com</p>
+      <p>Telefon Nummurs: 555-555-5555</p>
+    </div>
+  </footer>
+</body>
+</html>
